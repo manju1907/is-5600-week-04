7b0ea71f29c30871ffff5077157237d3a7cf6b58
manju1907 <lella.manjusri@gmail.com>
2025-11-17 03:05:12 +0000

    created delete and update apis and followed the modular flow to write code.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const fs = require('fs').promises
 const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+const middleware = require('./middleware')
+const api=require('./api')
+
 
 // Set the port
 const port = process.env.PORT || 3000
@@ -8,9 +12,20 @@ const port = process.env.PORT || 3000
 const app = express()
 // Register the public directory
 app.use(express.static(__dirname + '/public'));
-// register the routes
-app.get('/products', listProducts)
-app.get('/', handleRoot);
+
+// Require the middleware module
+
+// Register our upcoming middleware
+app.use(middleware.cors)
+app.use(bodyParser.json())
+app.get('/', api.handleRoot)
+app.get('/products', api.listProducts)
+app.get('/products/:id', api.getProduct)
+app.post('/products', api.createProduct)
+app.delete('/deleteProduct/:id',api.deleteProduct)
+app.put('/updateProduct/:id',api.updateProduct)
+
+
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
@@ -28,11 +43,9 @@ function handleRoot(req, res) {
  * @param {object} req
  * @param {object} res
  */
-async function listProducts(req, res) {
-  const productsFile = path.join(__dirname, 'data/full-products.json')
+async function listProducts (req, res) {
   try {
-    const data = await fs.readFile(productsFile)
-    res.json(JSON.parse(data))
+    res.json(await Products.list()) // Use the Products service
   } catch (err) {
     res.status(500).json({ error: err.message })
   }
